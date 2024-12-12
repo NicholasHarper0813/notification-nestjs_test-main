95df3fac22e9f124ff9ce300d42db88b6cada710
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:14:43 -0800

    Update nestjs-notification.service.ts

--- lib/nestjs-notification.service.ts
+++ lib/nestjs-notification.service.ts
@@ -1,26 +1,13 @@
+import { INestjsNotificationChannel, NestJsNotification } from './interfaces';
 import { Injectable, Type } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
-import { INestjsNotificationChannel, NestJsNotification } from './interfaces';
 
 @Injectable()
 export class NestjsNotificationService {
-  /**
-   * Resolve the channel needed to send the Notification
-   * @param channel
-   */
   private resolveChannel = (channel: Type<INestjsNotificationChannel>) =>
     this.moduleRef.create(channel);
 
-  /**
-   * @constructor
-   * @param {ModuleRef} moduleRef
-   */
   constructor(private moduleRef: ModuleRef) {}
-
-  /**
-   * Process a notification and send via designated channel
-   * @param {NestJsNotification} notification
-   */
   public send(notification: NestJsNotification): Promise<any> {
     const channels = notification.sendToChannels();
 
@@ -31,11 +18,6 @@ export class NestjsNotificationService {
     );
   }
 
-  /**
-   * Send notification on designated channel
-   * @param {NestJsNotification} notification
-   * @param {Type<INestjsNotificationChannel>} clientChannel
-   */
   private async sendOnChannel(
     notification: NestJsNotification,
     clientChannel: Type<INestjsNotificationChannel>,
