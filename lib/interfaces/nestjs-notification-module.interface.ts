7b9e5d84f19d4a6847dceb5cb05e6bb920990faf
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:15:07 -0800

    Update nestjs-notification-module.interface.ts

--- lib/interfaces/nestjs-notification-module.interface.ts
+++ lib/interfaces/nestjs-notification-module.interface.ts
@@ -1,24 +1,11 @@
 import { ModuleMetadata, Provider, Type } from '@nestjs/common';
 
-/**
- * @interface NestjsNotificationModuleOptionsFactory
- * @property createNestjsNotificationOptions()
- */
 export interface NestjsNotificationModuleOptionsFactory {
   createNestjsNotificationOptions():
     | Promise<NestjsNotificationModuleOptions>
     | NestjsNotificationModuleOptions;
 }
 
-/**
- * @interface NestjsNotificationModuleAsyncOptions
- * @extends {Pick<ModuleMetadata, 'imports'>}
- * @property useExisting
- * @property useClass
- * @property useFactory
- * @property inject
- * @property extraProviders
- */
 export interface NestjsNotificationModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
   useExisting?: Type<NestjsNotificationModuleOptionsFactory>;
   useClass?: Type<NestjsNotificationModuleOptionsFactory>;
@@ -29,7 +16,4 @@ export interface NestjsNotificationModuleAsyncOptions extends Pick<ModuleMetadat
   extraProviders?: Provider[];
 }
 
-/**
- * @interface NestjsNotificationModuleOptions
- */
 export type NestjsNotificationModuleOptions = null;
