b4b71d48f03a29883bba2a8cbef3a33ed8a733f4
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 23:15:25 -0800

    Update nestjs-notification.interface.ts

--- lib/interfaces/nestjs-notification.interface.ts
+++ lib/interfaces/nestjs-notification.interface.ts
@@ -1,21 +1,7 @@
-import { Type } from '@nestjs/common';
 import { INestjsNotificationChannel } from './nestjs-notification-channel.interface';
+import { Type } from '@nestjs/common';
 
-/**
- * @interface NestJsNotification
- * @property sendToChannels()
- * @property toPayload?()
- */
 export interface NestJsNotification {
-  /**
-   * Get the channels the notification should broadcast on.
-   * @returns {Type<INestjsNotificationChannel>[]} array
-   */
   sendToChannels(): Type<INestjsNotificationChannel>[];
-
-  /**
-   * Get the json representation of the notification.
-   * @returns {Record<string, any>}
-   */
   toPayload?(): Record<string, any>;
 }
